6fa414df6b5357aac2da642e744c4b4889558c14
Netsanet Gebremedhin <netsanet.gebremedhin@agenusbio.com>
2019-07-14 21:14:34 -0400

    Changes made to support the new clustering and dereplication algorith using usearch

--- bin/createPSAFiles.sh
+++ bin/createPSAFiles.sh
@@ -2,26 +2,25 @@
 
 hfrFile=$1
 referenceFile=$2
+outputDir=$3
 
 # Create a file that will be input to clustalO with column names precreated. 
 touch clustalO_input.txt	
 while read line
 
 do  
-	region=$(printf "%s\n" "$line"|awk -F'\t' '{ print $3 }')
+	region=$(printf "%s\n" "$line"|awk -F'\t' '{ print $2 }')
 	target_fasta_header=$(printf "%s\n" "$line"|awk -F'\t' '{ print $1 }')
-	frequency=$(printf "%s\n" "$line"|awk -F'\t'  '{ print $2 }')
-	targetSequence=$(printf "%s\n" "$line"|awk -F'\t'  '{ print $6 }')
-	mapStart=$(printf "%s\n" "$line"|awk -F'\t' '{ print $4 }')
+	targetSequence=$(printf "%s\n" "$line"|awk -F'\t'  '{ print $5 }')
+	mapStart=$(printf "%s\n" "$line"|awk -F'\t' '{ print $3 }')
 
-	echo $target_fasta_header
 
-	printf ">${target_fasta_header}\n${targetSequence}\n" > ${region}_${frequency}_targets_ref_psa_input.fa
+	printf ">${target_fasta_header}\n${targetSequence}\n" > ${outputDir}/${target_fasta_header}_targets_ref_psa_input.fa
 	
 	#TO-DO: Do not hard-code read length - 142 below
 	mapStop=`expr ${mapStart} + 141`
 	referenceSequence=$(cat ${referenceFile}|awk   "/${region}/{getline; print }")
 	referenceSubSequence=$(echo ${referenceSequence}|cut -c${mapStart}-${mapStop})
-	printf ">${region}\n${referenceSubSequence}\n" >> ${region}_${frequency}_targets_ref_psa_input.fa
-	printf "${region}_${frequency}_targets_ref_psa_input.fa\n" >> clustalO_input.txt
+	printf ">${region}\n${referenceSubSequence}\n" >> ${outputDir}/${target_fasta_header}_targets_ref_psa_input.fa
+	printf "${outputDir}/${target_fasta_header}_targets_ref_psa_input.fa\n" >> clustalO_input.txt
 done<$hfrFile
